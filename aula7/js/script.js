af8d470d4bff90c20a02c0be235abe3d64c1b6cc
DaviSuassunaunitins <davisuassuna@unitins.br>
2025-04-14 17:56:38 -0300

    version 6

--- aula7/js/script.js
+++ aula7/js/script.js
@@ -1,12 +1,12 @@
 document.getElementById("btnMulher").addEventListener("click", function(){
-    document.getElementById("header").style.backgroundColor = "red";
-    document.getElementById("footer").style.backgroundColor = "red";
-},false);
+    document.getElementsByTagName("header")[0].style.backgroundColor = "red";
+    document.getElementsByTagName("footer")[0].style.backgroundColor = "red";
+});
 document.getElementById("btnVoltar").addEventListener("click", function(){
-    document.getElementById("header").style.backgroundColor = "#333";
-    document.getElementById("footer").style.backgroundColor = "#333";
-},false);
+    document.getElementsByTagName("header")[0].style.backgroundColor = "#333";
+    document.getElementsByTagName("footer")[0].style.backgroundColor = "#333";
+});
 document.getElementById("btnHomem").addEventListener("click", function(){
-    document.getElementById("header").style.backgroundColor = "blue";
-    document.getElementById("footer").style.backgroundColor = "blue";
-},false);
+    document.getElementsByTagName("header")[0].style.backgroundColor = "blue";
+    document.getElementsByTagName("footer")[0].style.backgroundColor = "blue";
+});
